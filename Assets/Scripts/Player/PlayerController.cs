1fbe5fd57eac944a935bb5ac65c8942975a163e5
aprnna <121468657+aprnna@users.noreply.github.com>
2025-11-07 20:19:39 +0700

    player movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,17 +9,21 @@ namespace Player
         private Rigidbody2D _rigidbody2D;
         private Vector2 _movementInput;
         private GameManager _gameManager;
+        private Animator _animator;
 
         private void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
             _gameManager = GameManager.Instance;
+            _animator = GetComponent<Animator>();
             Debug.Log(_gameManager);
         }
 
         private void Update()
         {
             _movementInput = InputManager.Instance.PlayerInput.Movement.Get();
+            _animator.SetFloat("MoveX", _movementInput.x);
+            _animator.SetFloat("MoveY", _movementInput.y);
         }
 
         private void FixedUpdate()
